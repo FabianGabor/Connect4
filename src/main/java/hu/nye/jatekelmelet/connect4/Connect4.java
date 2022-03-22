9c6430d2fdb168e5d8eba745cb94b73f4d39e9fb
Fábián Gábor <gabor_fabian1@epam.com>
2022-03-22 21:05:27 +0100

    Connect4

--- src/main/java/hu/nye/jatekelmelet/connect4/Connect4.java
+++ src/main/java/hu/nye/jatekelmelet/connect4/Connect4.java
@@ -1,7 +1,41 @@
 package hu.nye.jatekelmelet.connect4;
 
+import hu.nye.jatekelmelet.connect4.controller.Game;
+import hu.nye.jatekelmelet.connect4.controller.Match;
+import hu.nye.jatekelmelet.connect4.model.Board;
+import hu.nye.jatekelmelet.connect4.model.Color;
+import hu.nye.jatekelmelet.connect4.model.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Connect4 {
+public class Connect4 implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Connect4.class);
+
+    @Override
+    public void run(String... args) {
+        LOG.info("STARTING ConnectFour");
+
+        int boardRowSize = 6;
+        int boardColumnSize = 7;
+
+        //Player pHuman = new Player("Human Player", Color.BLACK, "human");
+        Player pMinimax = new Player("Minimax Player",Color.BLACK, "minimax");
+        Player oMinimax = new Player("Minimax Opponent", Color.WHITE, "minimax");
+
+        Board connect4Board = new Board(boardRowSize, boardColumnSize);
+
+        Game game;
+        Match match;
+
+        game = new Game(connect4Board, pMinimax, oMinimax);
+        match = new Match(game);
+        match.play();
+        System.out.println(match);
+
+        LOG.info("GAME OVER");
+    }
 }
